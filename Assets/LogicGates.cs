f799844e34453144557ebb6671dcf2041dda752a
John van Terheijden <john@fastguide.eu>
2018-05-08 08:40:02 +0200

    Minor logging improvement.

--- Assets/LogicGates.cs
+++ Assets/LogicGates.cs
@@ -189,7 +189,7 @@ public class LogicGates : MonoBehaviour
             }
 
             Debug.LogFormat(
-                "[Logic Gates #{0}] Configuration: Inputs = {1}, Outputs = {2}, ({3} {4} {5}) {6} ({7} {8} {9}) = {10} {11} {12} = {13}{14}",
+                "[Logic Gates #{0}] Inputs = {1}, Outputs = {2}, ({3} {4} {5}) {6} ({7} {8} {9}) = {10} {11} {12} = {13}{14}",
                 _moduleId,
                 inputs,
                 outputs,
